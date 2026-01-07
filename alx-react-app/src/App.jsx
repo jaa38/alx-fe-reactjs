862b18c89b8a07af16ed6fbcec834e1ac2fb956b
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2026-01-07 01:13:26 +0100

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,15 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+      <p>I love to visit New York, and Tokyo</p>
+    </main>
+  );
+}
+
+export default MainContent;
