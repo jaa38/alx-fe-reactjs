314ef37220e0a43151d55c172568fb92fad53ebe
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2026-01-12 21:57:26 +0100

    Build a Simple Website with React

---
+++ my-company/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <Footer
+      style={{
+        marginTop: '40px',
+        padding: '10px',
+        textAlign: 'center',
+        backgroundColor: '#eee',
+      }}
+    >
+      <p>© 2026 My Company</p>
+    </Footer>
+  );
+}
+
+export default Footer;
